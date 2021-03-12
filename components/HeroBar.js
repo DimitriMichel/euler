cf188c9bbea577d4c8faacb23e48174a986a07ad
Dimitri PL <dimitripl@Dimitris-iMac.fios-router.home>
2021-03-12 00:47:17 -0500

    Hero Bar

--- components/HeroBar.js
+++ components/HeroBar.js
@@ -4,10 +4,12 @@ import styles from './HeroBar.module.css'
 const HeroBar = () => {
   return (
     <div className={styles.bar}>
+      <section id='universitybar'>
       <Image src='/the-university-of-sydney-3.svg' height={45} width={220} alt='University of Sydney logo'/>
       <Image src='/the-university-of-chicago.svg' height={40} width={200} alt='University of Chicago logo'/>
       <Image src='/the-university-of-sydney-3.svg' height={45} width={220} alt='University of Sydney logo'/>
       <Image src='/the-university-of-chicago.svg' height={40} width={200} alt='University of Chicago logo'/>
+      </section>
     </div>
   );
 };
