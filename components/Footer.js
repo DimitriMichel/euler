19893591abad72e72047f2d7e7f775b8f5cc818b
Dimitri PL <dimitripl@Dimitris-iMac.fios-router.home>
2021-03-21 23:44:51 -0400

    footer css

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../utility/Container";
+
+const Footer = () => {
+  return (
+    <Container>
+      <div className={}></div>
+    </Container>
+  );
+};
+
+export default Footer;
